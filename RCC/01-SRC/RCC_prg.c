abbb677ba4845ed135eef2d57ff13d3bec27c801
Islam Tarek <islamtarek0550@gmail.com>
2023-03-11 21:24:01 +0200

    Initial preparation for RCC APIs

--- RCC/01-SRC/RCC_prg.c
+++ RCC/01-SRC/RCC_prg.c
@@ -1,5 +1,45 @@
-/***********************************************************************************************************************************************************************/
-/*                                                              Author      : Islam Tarek                                                                              */
-/*                                                              SW_Module   : MCAL_STM32F103 RCC                                                                       */
-/*                                                              Last_Update : 06/03/2023                                                                               */
-/***********************************************************************************************************************************************************************/
+/*****************************************************************************
+ * @Author                : Islam Tarek<islamtarek0550@gmail.com>            *
+ * @CreatedDate           : 2023-03-11 21:14:58                              *
+ * @LastEditors           : Islam Tarek<islamtarek0550@gmail.com>            *
+ * @LastEditDate          : 2023-03-11 21:23:19                              *
+ * @FilePath              : RCC_prg.c                                        *
+ * @CopyRight             : Islam Tarek CopyRight                            *
+ ****************************************************************************/
+
+/**
+ * @brief  Includes Section
+ */
+
+#include "RCC_priv.h"
+#include "../02-CFG/RCC_cfg.h"
+#include "RCC_int.h"
+
+/**
+ * @brief  This API is used to initialize RCC driver with the given configurations.
+ * @return Error state which describes the state of API (Passed or failed).
+ */
+std_errorStatus_t MCAL_enu_Rcc_init(void)
+{
+
+}
+
+/**
+ * @brief This API is used to Enable the clock for the given Peripheral.
+ * @param local_enuPeripheral The peripheral whose clock will be enabled.
+ * @return Error state which describes the state of API (Passed or failed).
+ */
+std_errorStatus_t MCAL_enu_Rcc_enable (peripheral_t local_enuPeripheral)
+{
+
+}
+
+/**
+ * @brief This API is used to disable the clock for the given Peripheral.
+ * @param local_enuPeripheral The peripheral whose clock will be enabled.
+ * @return Error state which describes the state of API (Passed or failed). 
+ */
+std_errorStatus_t MCAL_enu_Rcc_disable(peripheral_t local_enuPeripheral)
+{
+    
+}
