e68f8796e36f97730f0b8297fc4b17050049df19
Islam Tarek <islam.tarek@valeo.com>
2023-03-29 15:47:31 +0200

    Prepare GPIO_prog file and adding of API documentation

--- GPIO/01-SRC/GPIO_prg.c
+++ GPIO/01-SRC/GPIO_prg.c
@@ -7,4 +7,109 @@
  * @CopyRight             : Islam Tarek CopyRight                            *
  ****************************************************************************/
 
+/**
+ * @section Includes
+ */
+#include "GPIO_priv.h"
+#include "GPIO_int.h"
+#include "RCC_int.h"
 
+/**
+ * @section Global Variables
+ */
+
+/**
+ * @brief GPIO Array
+ */
+static GPIO_reg_t *GPIO_ARR[GPIO_LAST] =
+    {
+        GPIO_A_REG,
+        GPIO_B_REG,
+        GPIO_C_REG,
+        GPIO_D_REG,
+        GPIO_E_REG};
+
+/**
+ * @section APIs Implementation
+ */
+
+/**
+ * @brief This API is used for GPIO Initialization
+ * @return Error state which describes the state of API (Passed or failed).
+ */
+std_errorStatus_t GPIO_init(void)
+{
+}
+
+/**
+ * @brief This API is used to set the Pin Mode.
+ * @note Modes are:
+ * @note - GPIO_PIN_MODE_AN_INPUT
+ * @note - GPIO_PIN_MODE_FLOATING_INPUT
+ * @note - GPIO_PIN_MODE_PD_INPUT
+ * @note - GPIO_PIN_MODE_PU_INPUT
+ * @note - GPIO_PIN_MODE_GP_PP_OUTPUT
+ * @note - GPIO_PIN_MODE_GP_OD_OUTPUT
+ * @note - GPIO_PIN_MODE_AF_PP_OUTPUT
+ * @note - GPIO_PIN_MODE_AF_OD_OUTPUT
+ * @param local_enuPort is the Port whose pin mode will be set (GPIO_x, x: (A -> E)).
+ * @param local_enuPin is the pin whose mode will be set (GPIO_PIN_x, x: (0 -> 15)).
+ * @param local_enuPinMode the mode to which the pin will set to.
+ * @return Error state which describes the state of API (Passed or failed).
+ */
+std_errorStatus_t GPIO_set_pin_mode(gpio_port_t local_enuPort, gpio_pin_t local_enuPin, gpio_pin_mode_t local_enuPinMode)
+{
+}
+
+/**
+ * @brief This API is used to set the Pin Level.
+ * @note Levels are:
+ * @note GPIO_PIN_LOW
+ * @note GPIO_PIN_HIGH
+ * @param local_enuPort is the Port whose pin level will be set (GPIO_x, x: (A -> E)).
+ * @param local_enuPin is the pin whose level will be set (GPIO_PIN_x, x: (0 -> 15)).
+ * @param local_enuPinLevel is the level to which the pin will be set.
+ * @return Error state which describes the state of API (Passed or failed).
+ */
+std_errorStatus_t GPIO_set_pin_level(gpio_port_t local_enuPort, gpio_pin_t local_enuPin, gpio_pin_level_t local_enuPinLevel)
+{
+}
+
+/**
+ * @brief This API is used to get the Pin Level.
+ * @note Levels are:
+ * @note GPIO_PIN_LOW
+ * @note GPIO_PIN_HIGH
+ * @param local_enuPort is the Port whose pin level will be got (GPIO_x, x: (A -> E)).
+ * @param local_enuPin is the pin whose level will be got (GPIO_PIN_x, x: (0 -> 15)).
+ * @param local_enuPinLevel is the level which will be got.
+ * @return Error state which describes the state of API (Passed or failed).
+ */
+std_errorStatus_t GPIO_get_pin_level(gpio_port_t local_enuPort, gpio_pin_t local_enuPin, gpio_pin_level_t *local_enuPinLevel)
+{
+}
+
+/**
+ * @brief This API is used to set the Pin max output speed.
+ * @note Output Speeds are:
+ * @note GPIO_PIN_MAX_OUTPUT_SPEED_2MHZ
+ * @note GPIO_PIN_MAX_OUTPUT_SPEED_10MHZ
+ * @note GPIO_PIN_MAX_OUTPUT_SPEED_50MHZ
+ * @param local_enuPort is the Port whose pin level will be got (GPIO_x, x: (A -> E)).
+ * @param local_enuPin is the pin whose level will be got (GPIO_PIN_x, x: (0 -> 15)).
+ * @param local_enuPinSpeed is the speed to which the Pin output speed will be set.
+ * @return Error state which describes the state of API (Passed or failed).
+ */
+std_errorStatus_t GPIO_set_pin_speed(gpio_port_t local_enuPort, gpio_pin_t local_enuPin, gpio_pin_speed_t local_enuPinSpeed)
+{
+}
+
+/**
+ * @brief This API is used to toggle Pin Level.
+ * @param local_enuPort is the Port whose pin level will be got (GPIO_x, x: (A -> E)).
+ * @param local_enuPin is the pin whose level will be got (GPIO_PIN_x, x: (0 -> 15)).
+ * @return Error state which describes the state of API (Passed or failed).
+ */
+std_errorStatus_t GPIO_toggle_pin_level(gpio_port_t local_enuPort, gpio_pin_t local_enuPin)
+{
+}
