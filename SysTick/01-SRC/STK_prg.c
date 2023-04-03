05a74a21e645f262a3e15ba478582fbbc811d038
Islam Tarek <islam.tarek@valeo.com>
2023-04-03 22:49:35 +0200

    Add Implementation of STK_init API

--- SysTick/01-SRC/STK_prg.c
+++ SysTick/01-SRC/STK_prg.c
@@ -2,7 +2,7 @@
  * @Author                : Islam Tarek<islam.tarek@valeo.com>               *
  * @CreatedDate           : 2023-04-01 23:11:23                              *
  * @LastEditors           : Islam Tarek<islam.tarek@valeo.com>               *
- * @LastEditDate          : 2023-04-03 22:32:18                              *
+ * @LastEditDate          : 2023-04-03 22:48:58                              *
  * @FilePath              : STK_prg.c                                        *
  ****************************************************************************/
 
@@ -32,7 +32,23 @@ static void (*ISR_FUNC)(void) = STD_NULL;
  */
 std_errorStatus_t STK_init(void)
 {
+    std_errorStatus_t error_state = STD_OK;
 
+    /* Set SysTick clock source */
+    #if ((STK_CLK_SRC == STK_AHB_CLK_DIV_BY_8) || (STK_CLK_SRC == STK_AHB_CLK))
+        ((STK_REG -> STK_CTRL).bits.CLKSOURCE) =  STK_CLK_SRC;
+    #else
+        error_state = STD_NOT_VALID_VALUE;
+    #endif
+
+    /* Set SysTick exception request state */
+    #if((STK_EXCEPTION_REQ_STATE == STK_EXCEPTION_REQ_DISABLE) || (STK_EXCEPTION_REQ_STATE == STK_EXCEPTION_REQ_ENABLE))
+        ((STK_REG -> STK_CTRL).bits.TICKINT) = STK_EXCEPTION_REQ_STATE;
+    #else
+        error_state = STD_NOT_VALID_VALUE;
+    #endif
+
+    return error_state;
 }
 
 /**
