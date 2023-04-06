793f325ebc7eecdf82227697e296d3134d30ae37
Islam Tarek <islam.tarek@valeo.com>
2023-04-06 16:28:05 +0200

    modify Implementation of STK_set_period_us API

--- SysTick/01-SRC/STK_prg.c
+++ SysTick/01-SRC/STK_prg.c
@@ -12,6 +12,7 @@
 #include "STK_priv.h"
 #include "STK_cfg.h"
 #include "STK_int.h"
+#include "RCC_int.h"
 
 /**
  * @section Global Variables
@@ -54,16 +55,38 @@ std_errorStatus_t STK_init(void)
 /**
  * @brief This API is used to set the period in micro seconds.
  * @param local_u32Pertiod is the period in micor seconds at which action will be taken.
- * @param local_u32Clk_KHZ is the SysTick clock in KHz.
  * @return Error state which describes the state of API (Passed or failed). 
- * @note The result of ((local_u32Pertiod * local_u32Clk_KHZ) / 1000) must not exceed the value 0x00FFFFFF.
+ * @note The result of ((local_u32Pertiod * stk_clk) / 1000) must not exceed the value 0x00FFFFFF.
  */
-std_errorStatus_t STK_set_period_us(u32 local_u32Clk_KHZ, u32 local_u32Pertiod)
+std_errorStatus_t STK_set_period_us(u32 local_u32Pertiod)
 {
     std_errorStatus_t error_state = STD_OK;
+    RCC_clk_t clocks = {
+        STD_u32MIN_VALUE,
+        STD_u32MIN_VALUE,
+        STD_u32MIN_VALUE,
+        STD_u32MIN_VALUE
+    };
+    u32 AHB_clk_KHZ = STD_u32MIN_VALUE;
+    u32 STK_clk_KHZ = STD_u32MIN_VALUE;
+
+    /* Get clocks' values */
+    error_state = RCC_get_clk_value(&clocks);
+
+    /* Get AHb clock value */
+    AHB_clk_KHZ = clocks.AHB_clk_KHZ;
+
+    /* Calculate systick clock value */
+    #if(STK_CLK_SRC == STK_AHB_CLK)
+        STK_clk_KHZ = AHB_clk_KHZ;
+    #elif(STK_CLK_SRC == STK_AHB_CLK_DIV_BY_8)
+        STK_clk_KHZ = AHB_clk_KHZ >> DIV_BY_8;
+    #else
+        error_state = STD_NOT_VALID_VALUE;
+    #endif
 
     /* Calculate the Load Value */
-    u64 STK_u32Load_Value = (((u64)(local_u32Clk_KHZ * (u64)local_u32Pertiod)) / CONVERT_FROM_MS_TO_US);
+    u64 STK_u32Load_Value = (((u64)(STK_clk_KHZ * (u64)local_u32Pertiod)) / CONVERT_FROM_MS_TO_US);
     
     /* Check if the calculated load Value in the accepted range of the load register or not */
     if( STK_u32Load_Value <= LOAD_REG_MAX_VALUE)
